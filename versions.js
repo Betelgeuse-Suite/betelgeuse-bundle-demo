994106aceefa201fc31d3f0b25441e016285b866
Gabriel C. Troia <catalin.troia@gmail.com>
2017-09-15 21:16:56 -0400

    Call it versions.js

---
+++ versions.js
@@ -0,0 +1,62 @@
+foo({
+  "1.0.0": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "2.0.0": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "2.0.1": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "3.0.0": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "3.0.1": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "3.0.2": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "3.0.3": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "3.0.4": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "3.1.0": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "3.2.0": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "4.0.0": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "4.0.1": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "4.0.2": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "4.0.3": {
+    "repository": "",
+    "timestamp": ""
+  },
+  "5.0.0": {
+    "repository": "",
+    "timestamp": ""
+  }
+});
